a7baf5da2d8aa5edfca159ffa9bbe2f1820f4ed8
Philosoph228 <philosoph228@gmail.com>
2023-12-01 13:35:42 +0500

    Update glsl vertex shader

--- vert.glsl
+++ vert.glsl
@@ -3,10 +3,12 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec2 aUV;
 
+uniform mat4 transform;
+
 out vec2 uv;
 
 void main() {
-  gl_Position = vec4(aPos, 1.0);
+  gl_Position = transform * vec4(aPos, 1.0);
   
   uv = aUV;
 }
